5622ea0bd099b848d5cb91f285d1d4eccb1232b6
dungntd99 <tiendungntd99@gmail.com>
2022-05-24 23:04:59 +0700

    Create project

---
+++ landing-page.js
@@ -0,0 +1,33 @@
+﻿ $(function(){
+   $("[data-hieuung='tooltip']").tooltip();
+
+   $(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+          loop:true,
+          margin:20,
+          nav:true,
+          responsive:{
+              576:{
+                  items:1
+              },
+              600:{
+                  items:3
+              },
+              1000:{
+                  items:4
+              }
+          }
+      })
+   });
+
+   $('.tieude').click(function(event) {
+   $(this).toggleClass('mauxanh');
+   // click cho nd hiện ra
+   $(this).next().slideToggle(500);
+   //chuyển nd
+   $('html,body').animate({scrollTop: ($(this).offset().top)},900);
+
+    });
+   $('.ndkhoi').slideUp();
+})  
+ 
